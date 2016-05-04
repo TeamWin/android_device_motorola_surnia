faa1ed572e0ebfe3cbf7828fffb330dfb48369f5
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-05-03 22:03:51 -0400

    surnia: switch to common TWRP device tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,41 +14,11 @@
 # limitations under the License.
 #
 
-LOCAL_PATH := device/motorola/surnia
+DEVICE_PATH := device/motorola/surnia
 
-TARGET_ARCH := arm
-TARGET_CPU_ABI  := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a7
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
+include device/motorola/msm8916-common/BoardConfigCommon.mk
 
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-
-TARGET_BOARD_PLATFORM := msm8916
-
-TARGET_BOOTLOADER_BOARD_NAME := MSM8916
-TARGET_NO_BOOTLOADER := true
-
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 vmalloc=400M androidboot.bootdevice=soc.0  movablecore=160M androidboot.selinux=permissive
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_RAMDISK_OFFSET := 0x01000000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000
-BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
-TARGET_KERNEL_SOURCE := kernel/motorola/msm8916
 TARGET_KERNEL_CONFIG := surnia-mini_defconfig
-
-BOARD_USES_QCOM_HARDWARE := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-TARGET_USES_MOTOROLA_LOG := true
-
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
-
-TARGET_RECOVERY_FSTAB := device/motorola/surnia/twrp.fstab
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072
@@ -58,20 +28,5 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1895825408  # 1851392 * 1024 mmcblk0p42
 BOARD_PERSISTIMAGE_PARTITION_SIZE := 8388608    # 8192 * 1024 mmcblk0p31
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 5368578048 # 5242752 * 1024 mmcblk0p44
 
-# don't take forever to wipe
-BOARD_SUPPRESS_SECURE_ERASE := true
-COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
-
-# Crypto
-TARGET_HW_DISK_ENCRYPTION := true
-TW_INCLUDE_CRYPTO := true
-
 # TWRP
 DEVICE_RESOLUTION := 540x960
-TARGET_RECOVERY_PIXEL_FORMAT := RGB_565
-TW_NEW_ION_HEAP := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-TW_SCREEN_BLANK_ON_BOOT := true
-TW_IGNORE_MAJOR_AXIS_0 := true
-
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
